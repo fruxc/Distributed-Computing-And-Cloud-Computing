290bc3718931dec97e1d87626f21021d5ae2a705
fruxc <hammadansari17@gmail.com>
2020-09-29 16:46:35 +0530

    Lab 2 updated

---
+++ lab_02/RPC/RPCEquation/Server.java
@@ -0,0 +1,46 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.StringTokenizer;
+
+public class Server {
+    public static void main(String args[]) throws IOException {
+
+        // Step 1: Establish the socket connection.
+        ServerSocket ss = new ServerSocket(4444);
+        System.out.println("Server ready");
+        Socket s = ss.accept();
+
+        // Step 2: Processing the request.
+        DataInputStream dis = new DataInputStream(s.getInputStream());
+        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+        while (true) {
+            // wait for input
+            String input = dis.readUTF();
+
+            if (input.equals("bye"))
+                break;
+
+            System.out.println("Equation received:-" + input);
+            int result;
+
+            // Use StringTokenizer to break the equation into operand and
+            // operation
+            StringTokenizer st = new StringTokenizer(input);
+
+            int oprnd1 = Integer.parseInt(st.nextToken());
+            int oprnd2 = Integer.parseInt(st.nextToken());
+
+            // perform the required operation.
+            result = (int) (Math.pow(oprnd1, 2) + Math.pow(oprnd2, 2) - 2 * oprnd1 * oprnd2);
+
+            System.out.println("Sending the result...");
+
+            // send the result back to the client.
+            dos.writeUTF(Integer.toString(result));
+        }
+    }
+}
